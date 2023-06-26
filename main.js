d528b7b150ad43fdf34007164e30ea087ccb0175
aghil-azimi <aghil.azimi992@gmail.com>
2023-06-26 23:29:40 +0200

    historisxhe-ort

---
+++ main.js
@@ -0,0 +1,60 @@
+const places = [
+    {
+        id: 1,
+        name: "Nasir al-Mulk-Moschee",
+        description: "Die Nasir al-Mulk-Moschee ist eine der schönsten Moscheen in Shiraz, deren Geschichte bis in die Qajar-Ära zurückreicht. Diese Moschee befindet sich in einem Viertel, das einst der Familie Qavam gehörte, und es gibt eine Reihe historischer Denkmäler aus dieser Zeit Heute befindet sich diese schöne Moschee, die auch als Rosa Moschee bekannt ist, als einer der beliebtesten Sehenswürdigkeiten in Shiraz",
+        src: "images/nasirolmolk.jpg"
+    },
+    {
+        id: 2,
+        name: "Eram Bagh",
+        description: "Der Eram-Garten ist eine der Touristenattraktionen von Shiraz und ein Beispiel für einen Garten-Koshk, der aufgrund seiner Zitrusbäume und der langen Allee mit prächtigen Zedern an seinen Seiten bei Touristen sehr beliebt ist. Neben dem Hauptpavillon aus der Qajar-Ära, der den zentralen Kern dieses Gartens bildet; Die großen und alten Seronazes haben seine Schönheit verdoppelt.",
+        src: "images/eram.jpg"
+    },
+    {
+        id: 3,
+        name: "خانه منطقی نژاد",
+        description: "Dieses Haus gilt als eines der historischen Gebäude der Qajar-Zeit und ist eine sehenswerte Sehenswürdigkeit in Shiraz. Es verfügt über einen königlichen Teil und verschiedene Räume im östlichen und westlichen Teil. Das Logische Haus von Nejad wurde 1352 als eines der Nationalwerke Irans registriert. Einige Teile des Musikmuseums umfassen die Musik der Nationen, iranische Instrumente, ein Musikstudio, einen Lehr- und Lernort, einen Archivraum und eine Vorstellung iranischer Musiker sowie ein Audio-Repository.",
+        src: "images/manteghinejad.jpg"
+    },
+    {
+        id: 4,
+        name: "Shapuri-Villa",
+        description: "Das Shapuri-Herrenhaus ist einer der schönsten Gärten und Sehenswürdigkeiten in Shiraz. Dieses Herrenhaus stammt aus der ersten Pahlavi-Zeit und ist eine Kombination aus iranischer und europäischer Architektur. In dieser Sammlung schaffen die natürlichen Effekte des Gartens und die wunderschöne Architektur des Gebäudes zusammen eine einzigartige Perspektive..",
+        src: "images/emaratShapoori.jpg"
+    },
+    {
+        id: 5,
+        name: "Narjestan Qavam Garden",
+        description: "Der Narenjestan-Garten, an dem ich vorbeikam, ist auch als Qavam-Garten bekannt und einer der schönsten Orte in Shiraz. Dieses Gebäude ist eines der Herrenhäuser aus der Qajar-Zeit und trägt diesen Namen wegen der Fülle an Orangenbäumen.",
+        src: "images/naranjeshtaneGhavam.jpg"
+    }
+]
+
+const imgs = document.querySelectorAll(".all-imgs img");
+const currentImage = document.querySelector(".current-img");
+const titelImage = document.querySelector(".image-title");
+const descImage = document.querySelector(".image-desc");
+
+console.log(imgs);
+
+imgs.forEach((image,index) => {
+    image.src = places[index].src
+
+    image.addEventListener('click',function(){
+        currentImage.src = places[index].src;
+        titelImage.innerHTML = places[index].name;
+        descImage.innerHTML = places[index].description;
+        currentImage.classList.add("animate-imgs");
+
+        setTimeout(function(){
+            currentImage.classList.remove("animate-imgs");
+        },400)
+
+    })
+})
+
+currentImage.src = places[0].src;
+titelImage.innerHTML = places[0].name;
+descImage.innerHTML = places[0].description;
+
